f5c82528db05a34e577040c416559947e569ca16
Antonio Petrov <petrov.antonio@gmail.com>
2024-07-11 20:13:05 +0300

    wip

--- web-store-dashboard/src/components/MainContent/MainContent.tsx
+++ web-store-dashboard/src/components/MainContent/MainContent.tsx
@@ -9,7 +9,7 @@ const MainContent: React.FC = () => {
   return (
     <div className='flex flex-col min-h-screen bg-website-background'>
       <div>
-        <div className='container mx-auto pt-3.5 flex items-center justify-between'>
+        <div className='container lg:ml-3.75 pt-3.5 flex items-center justify-between'>
           <div className='flex items-center space-x-2 text-sm text-gray-600'>
             <img src='/icons/house.svg' alt='house-icon' className='w-3 h-3' />
             <Breadcrumbs />
@@ -17,13 +17,13 @@ const MainContent: React.FC = () => {
           <TopBar />
         </div>
       </div>
-      <div className='container mx-auto'>
+      <div className='container '>
         <PageTitle title='Customer Maintenance' />
-        <div className='flex flex-col md:flex-row gap-6'>
-          <div className='w-full md:w-1/4 mb-6 md:mb-0'>
+        <div className='flex flex-col md:flex-row lg:flex-row gap-6'>
+          <div className='w-full md:w-1/3 lg:w-1/4 mb-6 md:mb-0'>
             <MaintenanceData />
           </div>
-          <div className='w-full md:w-3/4 flex flex-col'>
+          <div className='w-full md:w-2/3 lg:w-3/4 flex flex-col'>
             <MaintenanceDetails />
           </div>
         </div>
